aaf943e0458f74d7fd842e56d3a9e65a96d12863
Niall Liu <niall@aliyun.com>
2023-10-12 02:18:49 +0800

    update

--- baaso/install-all.sh
+++ baaso/install-all.sh
@@ -1,8 +1,3 @@
-mkdir -p /basso
-cp -rf ../basso-basso /basso/
-cp -rf ../basso-basso /basso/
-docker-compose -f docker-compose-dsc.yml -p basso up -d
-docker-compose -f docker-compose-msa.yml -p basso up -d
-docker-compose -f docker-compose-pmc.yml -p basso up -d
-docker-compose -f docker-compose-web.yml -p basso up -d
-
+install-ds.sh
+install-mb.sh
+install-pae.sh
