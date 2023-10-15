a0108fe7e7d5c09977dcb9a2d9d6853093485d07
Niall Liu <niall@aliyun.com>
2023-10-16 01:25:32 +0800

    support 1 layer

--- baaso/install-pae.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p /baaso
-mkdir -p /baaso/apisix/conf
-mkdir -p /baaso/apisix-dashboard/conf
-cp ./apisix/conf/config.yaml /baaso/apisix/conf/config.yaml
-cp ./apisix-dashboard/conf/conf.yaml /baaso/apisix-dashboard/conf/conf.yaml
-docker-compose -f docker-compose-pae.yml -p baaso up -d
-
