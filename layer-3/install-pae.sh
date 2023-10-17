f05bb002b9c543f1be6fbc0ef1237eb253c07b87
Niall Liu <niall@aliyun.com>
2023-10-18 06:14:20 +0800

    add nginx

--- layer-3/install-pae.sh
+++ layer-3/install-pae.sh
@@ -3,5 +3,6 @@ mkdir -p /baaso/apisix/conf
 mkdir -p /baaso/apisix-dashboard/conf
 cp ./apisix/conf/config.yaml /baaso/apisix/conf/config.yaml
 cp ./apisix-dashboard/conf/conf.yaml /baaso/apisix-dashboard/conf/conf.yaml
+cp -rf ./nginx /baaso/
 docker compose -f docker-compose-pae.yml -p baaso up -d
 
