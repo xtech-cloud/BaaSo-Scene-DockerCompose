63e377d2949cb7003f6808583aa83a76372bba22
Niall Liu <niall@aliyun.com>
2023-10-19 05:17:09 +0800

    update

--- layer-2/install-ma-pae.sh
+++ layer-2/install-ma-pae.sh
@@ -1,16 +1,7 @@
-mkdir -p /data/
-cp -rf ./baaso/BaaSo.Platform.DbMigrator /data/
-cp -rf ./baaso/BaaSo.Platform.AuthServer /data/
-cp -rf ./baaso/BaaSo.Platform.AdministrationService /data/
-cp -rf ./baaso/BaaSo.Platform.IdentityService /data/
-cp -rf ./baaso/BaaSo.Platform.SaasService /data/
-cp -rf ./baaso/BaaSo.Platform.CmsService /data/
-cp -rf ./baaso/BaaSo.Platform.WebGateway /data/
-cp -rf ./baaso/BaaSo.Platform.PublicWebGateway /data/
-cp -rf ./baaso/BaaSo.Platform.Web /data/
-cp -rf ./baaso/BaaSo.Platform.PublicWeb /data/
+mkdir -p /data/baaso
 
-cp -rf ./nginx /data/
+cp -rf ./baaso /data/baaso/
+cp -rf ./nginx /data/baaso/
 
 docker compose -f docker-compose-ma-pae.yml -p baaso up -d
 
