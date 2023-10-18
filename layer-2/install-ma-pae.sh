350fb01a94bcc8ae3d13e567da377f3acc2a3335
Niall Liu <niall@aliyun.com>
2023-10-19 04:43:46 +0800

    update

--- layer-2/install-ma-pae.sh
+++ layer-2/install-ma-pae.sh
@@ -1,16 +1,16 @@
-mkdir -p /baaso/baaso
-cp -rf ./baaso/BaaSo.Platform.DbMigrator /baaso/baaso/
-cp -rf ./baaso/BaaSo.Platform.AuthServer /baaso/baaso/
-cp -rf ./baaso/BaaSo.Platform.AdministrationService /baaso/baaso/
-cp -rf ./baaso/BaaSo.Platform.IdentityService /baaso/baaso/
-cp -rf ./baaso/BaaSo.Platform.SaasService /baaso/baaso/
-cp -rf ./baaso/BaaSo.Platform.CmsService /baaso/baaso/
-cp -rf ./baaso/BaaSo.Platform.WebGateway /baaso/baaso/
-cp -rf ./baaso/BaaSo.Platform.PublicWebGateway /baaso/baaso/
-cp -rf ./baaso/BaaSo.Platform.Web /baaso/baaso/
-cp -rf ./baaso/BaaSo.Platform.PublicWeb /baaso/baaso/
+mkdir -p /data/
+cp -rf ./baaso/BaaSo.Platform.DbMigrator /data/
+cp -rf ./baaso/BaaSo.Platform.AuthServer /data/
+cp -rf ./baaso/BaaSo.Platform.AdministrationService /data/
+cp -rf ./baaso/BaaSo.Platform.IdentityService /data/
+cp -rf ./baaso/BaaSo.Platform.SaasService /data/
+cp -rf ./baaso/BaaSo.Platform.CmsService /data/
+cp -rf ./baaso/BaaSo.Platform.WebGateway /data/
+cp -rf ./baaso/BaaSo.Platform.PublicWebGateway /data/
+cp -rf ./baaso/BaaSo.Platform.Web /data/
+cp -rf ./baaso/BaaSo.Platform.PublicWeb /data/
 
-cp -rf ./nginx /baaso/
+cp -rf ./nginx /data/
 
 docker compose -f docker-compose-ma-pae.yml -p baaso up -d
 
